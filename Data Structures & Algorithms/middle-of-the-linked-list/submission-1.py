0adefc8f0e8ca5473bf6911e3098d1e92b3be331
Pranav Bhendawade <pranavbhendawade@gmail.com>
2026-06-06 15:35:30 -0700

    Add: middle-of-the-linked-list - submission-1

---
+++ Data Structures & Algorithms/middle-of-the-linked-list/submission-1.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # First Method - calculate total, then mid and then traverse till mid 
+        # curr = head 
+        # count = 0 
+        # while curr: 
+        #     count += 1 
+        #     curr = curr.next
+        
+        # mid_index = count // 2 
+        # curr = head
+        # for _ in range(mid_index): 
+        #     curr = curr.next
+        # return curr 
+
+        # Second Method - slow and fast pointer 
+        slow, fast = head, head
+
+        while fast and fast.next: 
+            slow = slow.next 
+            fast = fast.next.next 
+
+        return slow
+        
+
+
+        
